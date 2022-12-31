13e50e9534b829dc1e0365f4cb5b2d753bcbd0a0
hiki0510 <121728266+hiki0510@users.noreply.github.com>
2023-01-01 02:50:43 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,14 @@ from distutils.sysconfig import PREFIX
 import discord
 from dotenv import load_dotenv
 import os
+import discord
+from discord.ext import commands
+from discord.utils import get
+from discord import FFmpegPCMAudio
+import asyncio
+import time
+import lxml
+from gtts import gTTS
 load_dotenv()
 
 PREFIX = os.environ['PREFIX']
@@ -14,6 +22,27 @@ client = discord.Client()
 async def on_ready():
     print(f'Logged in as {client.user}.')
 
+@bot.command(name="tts")
+async def tts(ctx, *args):
+    text = " ".join(args)
+    user = ctx.message.author
+    if user.voice != None:
+        try:
+            global vc
+            global entireText
+            vc = await ctx.message.author.voice.channel.connect()
+        except:
+            vc = ctx.voice_client
+            
+        sound = gTTS(text=text, lang="ko", slow=False)
+        sound.save("tts-audio.mp3")
+        if vc.is_playing():
+            vc.stop()
+            
+        vc.play(discord.FFmpegPCMAudio("tts-audio.mp3"))
+    else:
+        await ctx.send("없는 명령어 입니다")
+    
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -24,7 +53,6 @@ async def on_message(message):
 
     if message.content.startswith(f'{PREFIX}hello'):
         await message.channel.send('Hello!')
-
 
 try:
     client.run(TOKEN)
